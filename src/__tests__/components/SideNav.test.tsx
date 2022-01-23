0ead7865bff6fdb4b0ad47a1c57f89f68b78f70d
Terri <tmgast@gmail.com>
2022-01-23 10:16:53 +0900

    tiny update

--- src/__tests__/components/SideNav.test.tsx
+++ src/__tests__/components/SideNav.test.tsx
@@ -3,7 +3,16 @@ import SideNav from '../../components/SideNav';
 
 test('renders Nav', () => {
   render(<SideNav />);
-  const linkElement = screen.getByText(/Home/i);
-  expect(linkElement).toBeInTheDocument();
+  const homeLink = screen.getByText(/Home/i);
+  expect(homeLink).toBeInTheDocument();
+
+  const loginLink = screen.getByText(/Login/i);
+  expect(loginLink).toBeInTheDocument();
+
+  const frontPage = screen.getByText(/Front Page/i);
+  expect(frontPage).toBeInTheDocument();
+
+  const allPage = screen.getByText(/All/i);
+  expect(allPage).toBeInTheDocument();
 });
 
--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -3,7 +3,14 @@ import styled from 'styled-components';
 function SideNav() {
   return (
     <Nav>
-      <a href="/">Home</a>
+      <ul>
+        <li><a href="/">Login</a></li>
+        <li><a href="/">Home</a></li>
+      </ul>
+      <ul>
+        <li><a href="/">Front Page</a></li>
+        <li><a href="/">All</a></li>
+      </ul>
     </Nav>
   )
 };
@@ -12,11 +19,16 @@ export default SideNav;
 
 const Nav = styled.div`
   position: relative;
+  flex-basis: 10em;
   min-height: 100vh;
   order: 1;
   flex-grow: 1;
 
   background-color: #333;
+
+  ul {
+    list-style-type: none;
+  }
 
   a {
     size: 8em;
